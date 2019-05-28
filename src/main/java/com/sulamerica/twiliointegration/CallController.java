4b3f36e097ab03a1971a6a1a3b07747924267105
Elton dos Santos Magro <eltonm@ciandt.com>
2019-05-28 11:24:34 -0300

    Change to dynamic body in calls

--- src/main/java/com/sulamerica/twiliointegration/CallController.java
+++ src/main/java/com/sulamerica/twiliointegration/CallController.java
@@ -2,7 +2,6 @@ package com.sulamerica.twiliointegration;
 
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Call;
-import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
@@ -11,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 
 @RestController
 @RequestMapping("/v1/call")
@@ -24,10 +25,10 @@ public class CallController {
     }
 
     @PostMapping(value = "/send", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void send(@RequestBody CallBody body) throws URISyntaxException {
-
+    public void send(@RequestBody CallBody body) throws URISyntaxException, IOException {
         Call call = Call.creator(new PhoneNumber(body.getNumberTo()), new PhoneNumber(body.getNumberFrom()),
-                new URI("http://demo.twilio.com/docs/voice.xml")).create();
+                URI.create("http://q7817.mocklab.io/voice?message=" + URLEncoder.encode(body.getMessageBody(), "UTF-8")))
+                .create();
 
         System.out.println(call.getSid());
     }
